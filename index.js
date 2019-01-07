e66e847e5398d4bccb17abfbc3ed6246744e4c79
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-06 20:59:35 -0600

    finished tutorial 3 step 1; missing CSS

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function WebmailViewModel() {
     // Data
-    var self = this;
+    const self = this;
     self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+    self.chosenFolderId = ko.observable();
+    
+    // Behaviors
+    self.goToFolder = function(folder) {
+        self.chosenFolderId(folder);
+    };
 };
 
 ko.applyBindings(new WebmailViewModel());
