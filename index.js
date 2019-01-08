8438c5adfa0af21c73c99dc0511b0eb7820fc7b1
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-07 22:42:44 -0600

    finished viewing individual emails tutorial

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ function WebmailViewModel() {
     self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
     self.chosenFolderId = ko.observable();
     self.chosenFolderData = ko.observable();
+    self.chosenMailData = ko.observable();
 
     // Behaviours    
-    self.goToFolder = function(folder) { self.chosenFolderId(folder);
+    self.goToFolder = function(folder) { 
+    	self.chosenFolderId(folder);
+    	self.chosenMailData(null);
        $.get("/mail", {folder: folder}, self.chosenFolderData);
     };    
+
+    self.goToMail = function(mail) {
+    	self.chosenFolderId(mail.folder);
+    	self.chosenFolderData(null);
+    	$.get("/mail", {mailId: mail.id}, self.chosenMailData);
+    }
 
     // Show inbox by default
     self.goToFolder("Inbox");
