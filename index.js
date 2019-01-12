b44848d6331045ca988dcb7edeb3983846b7bd6e
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-12 11:43:12 -0600

    starter code for last tutorial, task list

--- index.js
+++ index.js
@@ -1,90 +1,27 @@
-ko.bindingHandlers.starRating = {
-  init: function(element, valueAccessor) {
-    $(element).addClass("starRating");
-    for (let i = 0; i < 5; i++) {
-      $("<span>").appendTo(element);
-    }
-    $("span", element).each(function(index) {
-      $(this)
-        .hover(
-          function() {
-            $(this)
-              .prevAll()
-              .add(this)
-              .addClass("hoverChosen");
-          },
-          function() {
-            $(this)
-              .prevAll()
-              .add(this)
-              .removeClass("hoverChosen");
-          }
-        )
-        .click(function() {
-          let observable = valueAccessor();
-          observable(index + 1);
-        });
-    });
-  },
-  update: function(element, valueAccessor) {
-    let observable = valueAccessor();
-    $("span", element).each(function(index) {
-      $(this).toggleClass("chosen", index < observable());
-    });
-  }
-};
-
-ko.bindingHandlers.jqButton = {
-  init: function(element) {
-    $(element).button();
-  },
-  update: function(element, valueAccessor) {
-    let currentValue = valueAccessor();
-    $(element).button("option", "disabled", currentValue.enable === false);
-  }
-};
-
-ko.bindingHandlers.fadeVisible = {
-  init: function(element, valueAccessor) {
-    // Lets the display fade in/out
-    let shouldDisplay = valueAccessor();
-    $(element).toggle(shouldDisplay);
-  },
-  update: function(element, valueAccessor) {
-    // Fades in/out on update
-    let shouldDisplay = valueAccessor();
-    shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
-  }
-};
-
-function Answer(text) {
-  this.answerText = text;
-  this.points = ko.observable(1);
+function Task(data) {
+  this.title = ko.observable(data.title);
+  this.isDone = ko.observable(data.isDone);
 }
 
-function SurveyViewModel(question, pointsBudget, answers) {
-  this.question = question;
-  this.pointsBudget = pointsBudget;
-  this.answers = $.map(answers, function(text) {
-    return new Answer(text);
+function TaskListViewModel() {
+  // Data
+  var self = this;
+  self.tasks = ko.observableArray([]);
+  self.newTaskText = ko.observable();
+  self.incompleteTasks = ko.computed(function() {
+    return ko.utils.arrayFilter(self.tasks(), function(task) {
+      return !task.isDone();
+    });
   });
-  this.save = function() {
-    alert("To do");
-  };
 
-  this.pointsUsed = ko.computed(function() {
-    let total = 0;
-    for (let i = 0; i < this.answers.length; i++)
-      total += this.answers[i].points();
-    return total;
-  }, this);
+  // Operations
+  self.addTask = function() {
+    self.tasks.push(new Task({ title: this.newTaskText() }));
+    self.newTaskText("");
+  };
+  self.removeTask = function(task) {
+    self.tasks.remove(task);
+  };
 }
 
-ko.applyBindings(
-  new SurveyViewModel("Which factors affect your technology choices?", 10, [
-    "Functionality, compatibility, pricing - all that boring stuff",
-    "How often it is mentioned on Hacker News",
-    "Number of gradients/dropshadows on project homepage",
-    "Totally believable testimonials on project homepage"
-  ])
-);
+ko.applyBindings(new TaskListViewModel());
